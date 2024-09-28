21cb6ce1b030206942c409a94cfde88a63ca2cde
Monzir Abdalrahman <60095683+monzir3bdo@users.noreply.github.com>
2024-09-28 18:48:32 +0300

    Setup Theme

--- lib/core/app/app.dart
+++ lib/core/app/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_direct_message/core/theme/app_theme.dart';
 import 'package:whatsapp_direct_message/screens/home_screen.dart';
 
 class App extends StatelessWidget {
@@ -6,8 +7,10 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeScreen(),
+    return MaterialApp(
+      home: const HomeScreen(),
+      theme: lightTheme(),
+      darkTheme: darkTheme(),
     );
   }
 }
