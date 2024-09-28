ccbb332c95e5c64eb6c3785658a4ad4442bf8581
Monzir Abdalrahman <60095683+monzir3bdo@users.noreply.github.com>
2024-09-29 02:56:23 +0300

    Lock The Preferred Orientations to Up only

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:whatsapp_direct_message/blocs/app/app_cubit.dart';
 import 'package:whatsapp_direct_message/blocs/history/get_history/history_bloc.dart';
@@ -7,18 +8,20 @@ import 'package:whatsapp_direct_message/core/database/hive_data_base.dart';
 
 void main() async {
   await HiveDatabase.instance.init();
-  runApp(
-    MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) =>
-              HistoryBloc()..add(const HistoryEvent.getContacts()),
-        ),
-        BlocProvider(
-          create: (context) => AppCubit(),
-        )
-      ],
-      child: const App(),
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
+    (_) => runApp(
+      MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) =>
+                HistoryBloc()..add(const HistoryEvent.getContacts()),
+          ),
+          BlocProvider(
+            create: (context) => AppCubit(),
+          )
+        ],
+        child: const App(),
+      ),
     ),
   );
 }
