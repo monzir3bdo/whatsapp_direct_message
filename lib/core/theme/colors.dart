6f6afac095728d537c664d09bd6580f23ac8b63e
Monzir Abdalrahman <60095683+monzir3bdo@users.noreply.github.com>
2024-09-28 18:24:20 +0300

    Add Theme Colors

--- lib/core/theme/colors.dart
+++ lib/core/theme/colors.dart
@@ -11,4 +11,10 @@ abstract class AppLightColors {
 abstract class AppDarkColors {
   static const Color backgroundColor = Color(0xff1E1E1E);
   static const Color primary = Color(0xff1E1E1E);
+  static const Color borderColor = Color(0xff4CAF50);
+  static const Color lightBackgroundColor = Color(0xff2A2A2A);
+  static const Color lightPrimary = Color(0xff71E57E);
+  static const Color grey = Color(0xffE0E0E0);
+  static const Color darkGrey = Color(0xff646464);
+  static const Color red = Color(0xffFF0000);
 }
