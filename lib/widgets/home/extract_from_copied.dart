17d92ec5275e16c6524cd6b2fec81337049c60fe
ZurraJanai <aiaaba99@gmail.com>
2024-09-29 22:53:06 +0300

    message working

--- lib/widgets/home/extract_from_copied.dart
+++ lib/widgets/home/extract_from_copied.dart
@@ -1,8 +1,10 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:whatsapp_direct_message/core/localization/lang_keys.dart';
 import 'package:whatsapp_direct_message/core/extensions/build_context_extension.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_direct_message/core/extensions/string_extension.dart';
 
+import '../../blocs/send/send_message_cubit.dart';
 import 'phone_number_widget.dart';
 
 class ExtractFromCopiedTextWidgets extends StatelessWidget {
@@ -20,6 +22,7 @@ class ExtractFromCopiedTextWidgets extends StatelessWidget {
           height: 10,
         ),
         TextFormField(
+          controller: context.read<SendMessageCubit>().copiedPhoneController,
           decoration: InputDecoration(
             hintText: context.translate(LangKeys.pasteTheTextHere),
           ),
--- lib/widgets/home/message_widgets.dart
+++ lib/widgets/home/message_widgets.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:whatsapp_direct_message/core/extensions/build_context_extension.dart';
 import 'package:whatsapp_direct_message/core/localization/lang_keys.dart';
 import 'package:whatsapp_direct_message/core/theme/app_text_styels.dart';
 import 'package:whatsapp_direct_message/core/theme/colors.dart';
+
+import '../../blocs/send/send_message_cubit.dart';
 
 class MessageWidgets extends StatelessWidget {
   const MessageWidgets({
@@ -24,6 +27,7 @@ class MessageWidgets extends StatelessWidget {
         SizedBox(
           height: context.height * 0.18,
           child: TextFormField(
+            controller: context.read<SendMessageCubit>().messageController,
             maxLines: 10,
             minLines: 10,
             decoration: InputDecoration(
