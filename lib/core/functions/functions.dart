30e12158bc4416fb4bb252755b1f6b1718b94ee0
ZurraJanai <aiaaba99@gmail.com>
2024-09-30 19:11:21 +0300

    remove spaces first

--- lib/core/functions/functions.dart
+++ lib/core/functions/functions.dart
@@ -1,12 +1,15 @@
 List<String> extractLongNumbers(String text) {
+  // Remove all spaces from the text
+  text = text.replaceAll(' ', '');
+
   // Regular expression to find sequences of digits that are 7 or more in length
   final RegExp regex = RegExp(r'\b\d{7,}\b');
-  
+
   // Find all matches in the text
   final matches = regex.allMatches(text);
-  
+
   // Convert matches to a list of strings
   List<String> longNumbers = matches.map((match) => match.group(0)!).toList();
-  
+
   return longNumbers;
 }
