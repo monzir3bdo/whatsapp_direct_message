e6abac02d8ed8ea2281f46a64475ac116a53466b
Monzir Abdalrahman <60095683+monzir3bdo@users.noreply.github.com>
2024-10-10 19:06:52 +0300

    Add Translation For App name

--- lib/core/theme/app_text_styels.dart
+++ lib/core/theme/app_text_styels.dart
@@ -1,58 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:whatsapp_direct_message/core/database/hive_data_base.dart';
 
 const String poppinsFont = 'Poppins';
 const String montserrat = 'Montserrat-Arabic';
 
 abstract class AppTextStyles {
-  static TextStyle light12 = TextStyle(
+  static TextStyle light12 = const TextStyle(
     fontSize: 12,
     fontWeight: FontWeight.w400,
-    fontFamily: HiveDatabase.instance.selectedLanguage!
-                .get(selectedLanguageKey, defaultValue: 'en') ==
-            'en'
-        ? poppinsFont
-        : montserrat,
     color: Colors.black,
   );
-  static TextStyle regular12 = TextStyle(
+  static TextStyle regular12 = const TextStyle(
     fontSize: 12,
     fontWeight: FontWeight.w400,
-    fontFamily: HiveDatabase.instance.selectedLanguage!
-                .get(selectedLanguageKey, defaultValue: 'en') ==
-            'en'
-        ? poppinsFont
-        : montserrat,
     color: Colors.black,
   );
-  static TextStyle medium14 = TextStyle(
+  static TextStyle medium14 = const TextStyle(
     fontSize: 14,
     fontWeight: FontWeight.w500,
-    fontFamily: HiveDatabase.instance.selectedLanguage!
-                .get(selectedLanguageKey, defaultValue: 'en') ==
-            'en'
-        ? poppinsFont
-        : montserrat,
     color: Colors.black,
   );
-  static TextStyle semiBold16 = TextStyle(
+  static TextStyle semiBold16 = const TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.w600,
-    fontFamily: HiveDatabase.instance.selectedLanguage!
-                .get(selectedLanguageKey, defaultValue: 'en') ==
-            'en'
-        ? poppinsFont
-        : montserrat,
     color: Colors.black,
   );
-  static TextStyle bold16 = TextStyle(
+  static TextStyle bold16 = const TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.bold,
-    fontFamily: HiveDatabase.instance.selectedLanguage!
-                .get(selectedLanguageKey, defaultValue: 'en') ==
-            'en'
-        ? poppinsFont
-        : montserrat,
     color: Colors.black,
   );
 }
